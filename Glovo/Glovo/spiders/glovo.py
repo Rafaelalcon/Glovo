df5b27026ab894aa3b17ee35fca176675c492c57
rafael <rafael.alcon@estudiant.fjaverianas.com>
2023-09-14 14:24:32 +0200

    Modificacion

--- Glovo/Glovo/spiders/glovo.py
+++ Glovo/Glovo/spiders/glovo.py
@@ -10,10 +10,10 @@ class GlovoSpider(scrapy.Spider):
     def start_requests(self):
         driver = webdriver.Chrome()
         driver.get("https://glovoapp.com/es/es")
-        titulo = driver.find_element(By.CSS_SELECTOR, 'h1.global-jumbotron__text-container__title').text
+        h1 = driver.find_element(By.CSS_SELECTOR, 'h1.global-jumbotron__text-container__title').text
         driver.close()
-        yield scrapy.Request(url="https://glovoapp.com/es/es", callback=self.parse, meta={'titulo': titulo})
+        yield scrapy.Request(url="https://glovoapp.com/es/es", callback=self.parse, meta={'titulo': h1})
         
     def parse(self, response):
-        titulo = response.meta['titulo']
-        print(titulo)
+        h1 = response.meta['titulo']
+        print(h1)
